95c871fc9a81dfd61c0141e83e19b36511bed6aa
Devin Connolly <dconnolly@triumf.ca>
2018-02-20 11:58:43 -0800

    Housekeeping and Sanitization
    File changes:
    - added gitignore and added appropriate files and dirs
    - renamed bashrcSegment to G4SONIKenv.sh commented unnecessary
      (redundant) lines
    - renamed README.txt -> README.md to use github's markdown
      and edited to reflect filename changes
    - renamed src/CrossCalc(*).hxx -> src/CrossCalc_*.hxx but left
      in src for now
    
    Housekeeping:
    - moved all emacs autosaves to untracked dir (emacs_autosaves)
      and copied to astro01:/group/astro/dragon/G4SONIK; original sim
      still lives at astro01:/local/astro/scat
      - astro01:/group/astro/dragon/G4SONIK is intended to be an
        updatable git repo but with original (now untracked) files
        contained within the repo, just in a more sane (and untracked!)
        place.
    - moved all rootfiles to untracked dir rootfiles; ditto above
    - moved all copies of dirs (e.g. - src\ \(copy2\) to untracked dir
      copies (and renamed w/out escape chars); ditto above

---
+++ G4SONIKenv.sh
@@ -0,0 +1,7 @@
+## Edit the paths below appropriately
+## GEANT4 ##
+source /usr/local/GEANT4/geant4.9.6.p04-install/bin/geant4.sh
+
+## ROOT ##
+# export ROOTSYS=/usr/local/cern-root/root
+# source $ROOTSYS/bin/thisroot.sh
